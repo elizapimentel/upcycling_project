877ceb6f4f14111d15a8629ef43944657e66b950
elizapimentel <elizapimentel@hotmail.com>
2024-10-24 11:47:50 -0300

    chore: add entity imports

--- src/members/members.module.ts
+++ src/members/members.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MembersService } from './services/members.service';
 import { MembersController } from './controllers/members.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MemberEntity } from './entities/member.entity';
+import { AddressEntity } from './entities/address.entity';
 
 @Module({
   controllers: [MembersController],
   providers: [MembersService],
+  imports: [
+    TypeOrmModule.forFeature([MemberEntity, AddressEntity]),
+  ],
 })
 export class MembersModule {}

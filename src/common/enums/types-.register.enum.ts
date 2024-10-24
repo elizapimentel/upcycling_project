9fe642efd0a528ef7cdee11609f8424a0340597f
elizapimentel <elizapimentel@hotmail.com>
2024-10-24 16:57:58 -0300

    feat: add enums

---
+++ src/common/enums/types-.register.enum.ts
@@ -0,0 +1,11 @@
+export enum TypeProduct {
+    DONATION = 'donation',
+    SALE = 'sale',
+}
+
+export enum TypeMember {
+    DONOR = 'donor',
+    ARTISAN = 'artisan',
+    SHOP_OWNER = 'shop_owner',
+}
+

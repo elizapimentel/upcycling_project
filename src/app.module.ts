316fdeaca484e82d85ed9f7224a436b5ec36352d
elizapimentel <elizapimentel@hotmail.com>
2024-10-24 14:50:07 -0300

    feat: add products module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MembersModule } from './members/members.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [MembersModule],
+  imports: [MembersModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })

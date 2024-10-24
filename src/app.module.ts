54df983cc9736f7032e56d11997addfaccb63e54
elizapimentel <elizapimentel@hotmail.com>
2024-10-24 16:57:47 -0300

    chore: add dotenv config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MembersModule } from './members/members.module';
 import { ProductsModule } from './products/products.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MembersModule, ProductsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }), 
+    MembersModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })

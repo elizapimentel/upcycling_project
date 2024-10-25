a0085819fa965594663e1877208e6428933b101b
elizapimentel <elizapimentel@hotmail.com>
2024-10-25 16:35:45 -0300

    feat: add bcrypt config

--- src/members/entities/member.entity.ts
+++ src/members/entities/member.entity.ts
@@ -1,8 +1,9 @@
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { BeforeInsert, Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { IRegister } from '../../common/interfaces/IRegister';
 import { AddressEntity } from './address.entity';
 import { TypeMember } from '../../common/enums/types-.register.enum';
 import { ProductsEntity } from '../../products/entities/products.entity';
+import * as bcrypt from 'bcrypt';
 
 @Entity({ name: 'members' })
 export class MemberEntity implements IRegister {
@@ -41,5 +42,15 @@ export class MemberEntity implements IRegister {
 
     @OneToMany(() => ProductsEntity, product => product.owner)
     products: ProductsEntity[];
+
+    @BeforeInsert()
+    async hashPassword() {
+        const salt = await bcrypt.genSalt();
+        this.password = await bcrypt.hash(this.password, salt);
+    }
+
+    async comparePassword(attempt: string): Promise<boolean> {
+        return await bcrypt.compare(attempt, this.password);
+    }
 
 }

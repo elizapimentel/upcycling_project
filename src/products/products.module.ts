ad2a142dea7ad41f30e11aecc8c579883961405a
elizapimentel <elizapimentel@hotmail.com>
2024-10-28 22:27:16 -0300

    feat: add TypeOrm entities

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ProductsService } from './services/products.service';
 import { ProductsController } from './controllers/products.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductsEntity } from './entities/products.entity';
+import { MemberEntity } from '../members/entities/member.entity';
 
 @Module({
   controllers: [ProductsController],
   providers: [ProductsService],
+  imports: [
+    TypeOrmModule.forFeature([ProductsEntity, MemberEntity]),
+  ],
 })
 export class ProductsModule {}

5df5ab70e7128fab1d3c312628fbfe9d8adee702
elizapimentel <elizapimentel@hotmail.com>
2024-10-28 22:25:25 -0300

    feat: add ProductsEntity

--- src/members/members.module.ts
+++ src/members/members.module.ts
@@ -4,12 +4,13 @@ import { MembersController } from './controllers/members.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MemberEntity } from './entities/member.entity';
 import { AddressEntity } from './entities/address.entity';
+import { ProductsEntity } from '../products/entities/products.entity';
 
 @Module({
   controllers: [MembersController],
   providers: [MembersService],
   imports: [
-    TypeOrmModule.forFeature([MemberEntity, AddressEntity]),
+    TypeOrmModule.forFeature([MemberEntity, AddressEntity, ProductsEntity]),
   ],
 })
 export class MembersModule {}

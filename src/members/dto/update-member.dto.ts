160b38429bf866ac6c26e26c11caa455b83d7fd2
elizapimentel <elizapimentel@hotmail.com>
2024-10-30 23:59:20 -0300

    refactor: change dto

--- src/members/dto/update-member.dto.ts
+++ src/members/dto/update-member.dto.ts
@@ -1,4 +1,32 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateMemberDto } from './create-member.dto';
+import { IsString, IsOptional, IsPhoneNumber, IsEmail, IsStrongPassword } from "class-validator";
 
-export class UpdateMemberDto extends PartialType(CreateMemberDto) {}
+
+export class UpdateMemberDto {
+    @IsString()
+    @IsOptional()
+    fullName?: string;
+
+    @IsString()
+    @IsOptional()
+    businessName?: string;
+
+    @IsPhoneNumber()
+    @IsOptional()
+    phone?: string;
+
+    @IsEmail()
+    @IsOptional()
+    email?: string;
+
+    @IsStrongPassword()
+    @IsOptional()
+    password?: string;
+
+    @IsString()
+    @IsOptional()
+    socialMedia?: string;
+
+    @IsString()
+    @IsOptional()
+    zipCode?: string;
+}

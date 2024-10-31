5acab2d45aa8f29f09c01820426d039dd417257d
elizapimentel <elizapimentel@hotmail.com>
2024-10-30 23:09:59 -0300

    feat: add create product dto

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,26 @@
-export class CreateProductDto {}
+import { IsEnum, IsNotEmpty, IsNumber, IsOptional, IsString } from "class-validator";
+import { TypeProduct } from "../../common/enums/types-.register.enum";
+
+export class CreateProductDto {
+
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+
+    @IsString()
+    @IsNotEmpty()
+    description: string;
+
+    @IsEnum(TypeProduct)
+    @IsNotEmpty()
+    productType: TypeProduct;
+
+    @IsOptional()
+    @IsNumber()
+    price?: number;
+
+    @IsString()
+    @IsNotEmpty()
+    contact: string;
+    
+}

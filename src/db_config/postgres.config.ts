289cb263d9bbd7b7a4ecaf6bfa6e32315e004b92
elizapimentel <elizapimentel@hotmail.com>
2024-10-30 23:05:45 -0300

    fix: remove migrations

--- src/db_config/postgres.config.ts
+++ src/db_config/postgres.config.ts
@@ -6,12 +6,11 @@ config();
 export const AppDataSource = new DataSource({
     type: 'postgres',
     host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT, 10),
+    port: parseInt(process.env.DB_PORT, 10) || 5432,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    entities: [__dirname + '/../**/entities/*.entity{.ts,.js}'],
-    migrations: [__dirname + '/../db_config/migrations/*{.ts,.js}'],
-    synchronize: false,
+    entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+    synchronize: true,
     logging: true,
 });
